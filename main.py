13acdd62730195305ec28ff5201e13e4b4e9aec2
filipj169 <jamrichfilip16@gmail.com>
2023-12-02 21:16:29 +0100

    simulation of moves

--- main.py
+++ main.py
@@ -1,44 +1,41 @@
-# Časť prvá
+import random
+
+def najdi_poziciu_hraca(sachovnica, hrac):
+    suradnice = ["", ""]
+    for i, riadok in enumerate(sachovnica):
+        if hrac in riadok:
+            suradnice[0] = i
+            suradnice[1] = riadok.index(hrac)
+    return suradnice
 
 def gensachovnicu(n):
     # Kotrola -> vstup nesmie byť párne číslo
-    # Ak je vstup párne číslo - vráti to error a rovno upozorní používateľa ako má postupovať
     if n % 2 == 0:
         raise ValueError("Rozmer šachovnice musí byť nepárne číslo. Zadajte prosím číslo, ktoré nie je párne.")
     
-    # Zadefinovanie šachovnice
     sachovnica = []
 
-    # Vytvoríme prázdnu šachovnicu, ktorá bude reprezentovaná riadkami
-    # Riadky sa budú plniť na základe určitej logiky v kóde nižšie
     for i in range(n):
         riadok_sachovnice = []
         for j in range(n):
             riadok_sachovnice.append(" ")
         sachovnica.append(riadok_sachovnice)
 
-    # Podľa tejto logiky sa najskôr vypočíta šírka ramena
-    # Na základe toho program zistí, že v akej pozícii bude "medzera" medzi ramenami
     sirka_ramena = (n - 3) // 2
+
     for i in range(sirka_ramena, sirka_ramena + 3):
-        # Do šachovnice sa pridajú hviezdičky, ktoré hraničia s "borderom" šachovnice
         sachovnica[0][i] = "*"
         sachovnica[n - 1][i] = "*"
         sachovnica[i][0] = "*"
         sachovnica[i][n - 1] = "*"
     
     for i in range(1, n - 1):
-        # Do šachovnice sa pridajú políčka pre domček
         sachovnica[i][sirka_ramena + 1] = "D"
         sachovnica[sirka_ramena + 1][i] = "D"
 
-    # Do stredu šachovnice sa pridá bod "X"
     sachovnica[sirka_ramena + 1][sirka_ramena + 1] = "X"
 
     for i in range(sirka_ramena):
-        # V tomto for cykle sa do šachovnice pridajú všetky jednotlivé symboly "*"
-
-        # Postupuje sa najskôr podľa osi x
         # X
         sachovnica[sirka_ramena][i + 1] = "*"
         sachovnica[sirka_ramena][sirka_ramena + 2 + i] = "*"
@@ -46,7 +43,6 @@ def gensachovnicu(n):
         sachovnica[sirka_ramena + 2][i + 1] = "*"
         sachovnica[sirka_ramena + 2][sirka_ramena + 2 + i] = "*"
 
-        # Potom podľa osi y
         # Y
         sachovnica[sirka_ramena - i][sirka_ramena] = "*"
         sachovnica[sirka_ramena - i][sirka_ramena + 2] = "*"
@@ -54,18 +50,50 @@ def gensachovnicu(n):
         sachovnica[sirka_ramena + 2 + i][sirka_ramena] = "*"
         sachovnica[sirka_ramena + 2 + i][sirka_ramena + 2] = "*"
 
-    # Dokončenú šachovnicu "vrátime" z funkcie von pre ďalšie použitie    
+    # Číslovanie riadkov
+    for i, riadok in enumerate(sachovnica):
+        riadok.insert(0, f"{i % 10 :2d}")
+    
+    # Číslovanie stĺpcov
+    stlpce = []
+    for i in range(n):
+        stlpce.append(f"{i % 10:2d}".replace(" ", ""))
+    stlpce.insert(0, "  ")
+    sachovnica.insert(0, stlpce)
+
+    # Nastavenie pozície hráča
+    sachovnica[1][sirka_ramena + 3] = "A"
+    suradnica_pred_domcekom = sachovnica[1].index(sachovnica[1][sirka_ramena + 2])
+    pozicia_pred_domcekom = sachovnica[1][suradnica_pred_domcekom]
+
+    # Simulácia pohybu - TODO: Spraviť funkciu
+    hod_kockou = random.randint(1, 6)
+    hod_kockou = 8
+    print(f"Hráč hodil číslo {hod_kockou}")
+    print("\n")
+    
+    for i in range(hod_kockou):
+        pozicia_hraca = najdi_poziciu_hraca(sachovnica, "A")
+        sachovnica[pozicia_hraca[0]][pozicia_hraca[1]] = "*"
+
+        # Kam má hráč zabočiť
+        if sachovnica[pozicia_hraca[0] + 1][pozicia_hraca[1]] == "D" and sachovnica[pozicia_hraca[0]][pozicia_hraca[1] + 1] == "*":
+            sachovnica[pozicia_hraca[0]][pozicia_hraca[1] + 1] = "A" 
+        
+        elif sachovnica[pozicia_hraca[0] + 1][pozicia_hraca[1]] == "*":
+            sachovnica[pozicia_hraca[0] + 1][pozicia_hraca[1]] = "A"
+       
+        # TODO: Ak je nad hráčom domček, ide doľava
+
     return sachovnica
 
 
 def tlacsachovnicu(sachovnica):
-    for riadok in sachovnica:
-        print(riadok)
+   for riadok in sachovnica:
+        print(" ".join(riadok))
 
-dokoncena_sachovnica = gensachovnicu(9)
+velkost_sachovnice = int(input("Zadajte veľkosť šachovnice: "))
+dokoncena_sachovnica = gensachovnicu(velkost_sachovnice)
 tlacsachovnicu(dokoncena_sachovnica)
 
-# Časť druhá
-
-def simulacia_pohybu(sachovnica):
-    pass
+    
